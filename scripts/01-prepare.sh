eb349079d8ad039c47260257759fc2478244fd1a
v-aisac <v-aisac@digitalocean.com>
2022-11-17 16:22:48 +0200

    Added initialization for dbaas

--- scripts/01-prepare.sh
+++ scripts/01-prepare.sh
@@ -2,6 +2,32 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
+PG_HOST=/var/run/postgresql
+PG_PORT=5432
+PG_PASS=""
+PG_USER=postgres
+PG_DB=postgres
+
+# Set up postgres dbaas
+if [ -f "/root/.digitalocean_dbaas_credentials" ] && [ "$(sed -n "s/^db_protocol=\"\(.*\)\"$/\1/p" /root/.digitalocean_dbaas_credentials)" = "postgresql" ];
+then
+  # grab all the data from the dbaas credentials file
+  PG_HOST=$(sed -n "s/^db_host=\"\(.*\)\"$/\1/p" /root/.digitalocean_dbaas_credentials)
+  PG_PORT=$(sed -n "s/^db_port=\"\(.*\)\"$/\1/p" /root/.digitalocean_dbaas_credentials)
+  PG_USER=$(sed -n "s/^db_username=\"\(.*\)\"$/\1/p" /root/.digitalocean_dbaas_credentials)
+  PG_DB=$(sed -n "s/^db_database=\"\(.*\)\"$/\1/p" /root/.digitalocean_dbaas_credentials)
+  PG_PASS=$(sed -n "s/^db_password=\"\(.*\)\"$/\1/p" /root/.digitalocean_dbaas_credentials)
+
+  # wait for db to become available
+  echo -e "\nWaiting for your database to become available (this may take a few minutes)"
+  while ! pg_isready -h "$PG_HOST" -p "$PG_PORT"; do
+     printf .
+     sleep 2
+  done
+
+  echo -e "\nDatabase available!\n" # Should we include echo here?
+fi
+
 cloud-init status --wait \
   && apt -qqy update \
   && apt -qqy -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' full-upgrade \
@@ -19,4 +45,4 @@ cloud-init status --wait \
   && corepack enable \
   && yarn set version stable \
   && adduser --disabled-login --gecos '' mastodon \
-  && sudo -u postgres psql -c "CREATE USER mastodon CREATEDB;"
+  && sudo -u PGPASSWORD=$PG_PASS psql -h $PG_HOST -p $PG_PORT -U $PG_USER -d $PG_DB "CREATE USER mastodon CREATEDB;" --set=sslmode=require
